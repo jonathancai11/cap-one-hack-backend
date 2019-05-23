011d38f890ceff4a739dabcf43c8b2a06b8d4645
Jonathan Cai <caimjonathan@gmail.com>
2019-05-23 11:41:41 -0400

    initial api routes and .gitignore

--- api/_01_manual_response_class.py
+++ api/_01_manual_response_class.py
@@ -4,28 +4,39 @@ from .utils import JSON_MIME_TYPE, search_book
 
 app = Flask(__name__)
 
-books = [{
-    'id': 33,
-    'title': 'The Raven',
-    'author_id': 1
-}]
+img_resp = {
+    "Received image": "Goood stuff man",
+}
+
+hist_resp = {
+    "Here are all of the history": "Goood stuff man",
+}
 
 
-@app.route('/book')
-def book_list():
+insights_resp = {
+    "Here are all of the insights": "Goood stuff man",
+}
+
+
+
+@app.route('/api/img')
+def img_upload():
     response = Response(
-        json.dumps(books), status=200, mimetype=JSON_MIME_TYPE)
+        json.dumps(img_resp), status=200, mimetype=JSON_MIME_TYPE)
     return response
 
 
-@app.route('/book/<int:book_id>')
-def book_detail(book_id):
-    book = search_book(books, book_id)
-    if book is None:
-        abort(404)
+@app.route('/api/history')
+def book_detail():
+    response = json.dumps(hist_resp)
+    return response, 200, {'Content-Type': JSON_MIME_TYPE}
 
-    content = json.dumps(book)
-    return content, 200, {'Content-Type': JSON_MIME_TYPE}
+
+@app.route('/api/insights')
+def insights():
+    response = Response(
+        json.dumps(insights_resp), status=200, mimetype=JSON_MIME_TYPE)
+    return response
 
 
 @app.errorhandler(404)
